4dd12f8354e10a0345fc34ef2d3761c01a28e0fc
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-02-17 18:22:31 +0200

    Created Toolbar variable toolbar

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/DisplayActivity.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/DisplayActivity.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
-        TextView textView = findViewById(R.id.textView);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+
+        /*TextView textView = findViewById(R.id.textView);
 
         Intent intent = getIntent();
         String message1 = intent.getStringExtra(MainActivity.KEY_MESSAGE);
-        textView.setText(message1);
+        textView.setText(message1);*/
     }
 }
