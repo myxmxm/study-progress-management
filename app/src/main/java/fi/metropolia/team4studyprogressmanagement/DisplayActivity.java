278675c0bd3538bc18b9aec0a283ead7fd91dea2
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-02-17 23:33:55 +0200

    added tabLayout with fragment functionality to DisplayActivity

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/DisplayActivity.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/DisplayActivity.java
@@ -1,15 +1,31 @@
 package fi.metropolia.team4studyprogressmanagement;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabItem tabItemCourse, tabItemTools, tabItemStatistic;
+    private PagerAdapter pagerAdapter;
 
 
     @Override
@@ -17,10 +33,35 @@ public class DisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
-        toolbar = findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent intent = getIntent();
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabItemCourse = (TabItem) findViewById(R.id.tabItemCourse);
+        tabItemTools = (TabItem) findViewById(R.id.tabItemTools);
+        tabItemStatistic = (TabItem) findViewById(R.id.tabItemStatistic);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(),FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         /*TextView textView = findViewById(R.id.textView);
 
