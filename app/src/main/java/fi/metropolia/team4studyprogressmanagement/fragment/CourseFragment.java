c3979c05fe02b323bc12b47702ced80fbe44f466
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-02-18 14:58:19 +0200

    Removed some auto-generated methods from all Fragments, and made few small tests in CourseFragment.

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/CourseFragment.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/CourseFragment.java
@@ -1,66 +1,47 @@
 package fi.metropolia.team4studyprogressmanagement.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import fi.metropolia.team4studyprogressmanagement.MainActivity;
+import fi.metropolia.team4studyprogressmanagement.PagerAdapter;
 import fi.metropolia.team4studyprogressmanagement.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CourseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CourseFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CourseFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CourseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CourseFragment newInstance(String param1, String param2) {
-        CourseFragment fragment = new CourseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button button = (Button) getActivity().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.TAG,"clicked");
+            }
+        });
+
+        TextView textView = getView().findViewById(R.id.textView);
+        //String message = "hello xiaoming";
+
+        SharedPreferences preGet = getActivity().getSharedPreferences("USER_DATE", Context.MODE_PRIVATE);
+        String message = preGet.getString(MainActivity.KEY_USERNAME,"");
+        textView.setText(message);
+
+
     }
 }
