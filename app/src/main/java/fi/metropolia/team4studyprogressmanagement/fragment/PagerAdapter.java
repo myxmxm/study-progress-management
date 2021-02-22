040fcb096d847a0086e430ccf09f95fde9c7587e
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-02-22 23:41:01 +0200

    Moved PagerAdapter.java to fragment package

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
@@ -1,4 +1,4 @@
-package fi.metropolia.team4studyprogressmanagement;
+package fi.metropolia.team4studyprogressmanagement.fragment;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
