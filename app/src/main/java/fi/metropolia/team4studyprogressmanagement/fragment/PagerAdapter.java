d43c4e214ca7c589b0529aeb644305a0dd0f08fb
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-03-03 14:17:39 +0200

    Changed return new CourseFragment(); to return new EditFragment();

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
@@ -5,10 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import fi.metropolia.team4studyprogressmanagement.fragment.CourseFragment;
-import fi.metropolia.team4studyprogressmanagement.fragment.StatisticFragment;
-import fi.metropolia.team4studyprogressmanagement.fragment.ToolsFragment;
-
 public class PagerAdapter extends FragmentPagerAdapter {
 
     private int numberOfTabs;
@@ -23,9 +19,9 @@ public class PagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         switch (position){
             case 0:
-                return new CourseFragment();
+                return new EditFragment();
             case 1:
-                return new ToolsFragment();
+                return new SearchFragment();
             case 2:
                 return new StatisticFragment();
             default:
