a52ae5f9e2983550eb990c153f4507cc896620ef
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-03-03 14:26:57 +0200

    Implemented Room database for this class

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/CourseDetailsActivity.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/CourseDetailsActivity.java
@@ -4,26 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class CourseDetailsActivity extends AppCompatActivity {
 
-    private EditText courseName, courseSemester, courseGrade, courseCredit, courseExtraInfo ;
-    private Button buttonUpdateCourse;
+    private EditText courseName, courseSemester, courseGrade, courseCredit, courseDetail ;
+    private Button buttonUpdateCourse, buttonDeleteCourse;
+    private TextView courseId;
+    private CourseDatabase courseDatabase;
+    private CourseDao courseDao;
+    int listCourseId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
 
-        initialization();
 
+        courseDatabase = CourseDatabase.getInstance(this);
+        courseDao = courseDatabase.getCourseDao();
+
+        initialization();
         getCourseInfo ();
 
-
-
+        buttonUpdateCourse.setOnClickListener(updateCourse);
+        buttonDeleteCourse.setOnClickListener(deleteCourse);
 
     }
 
@@ -32,25 +45,67 @@ public class CourseDetailsActivity extends AppCompatActivity {
         courseSemester = (EditText) findViewById(R.id.courseSemester);
         courseGrade = (EditText) findViewById(R.id.courseGrade);
         courseCredit = (EditText) findViewById(R.id.courseCredit);
-        courseExtraInfo = (EditText) findViewById(R.id.courseExtraInfo);
-
+        courseDetail = (EditText) findViewById(R.id.courseExtraInfo);
+        courseId = (TextView) findViewById(R.id.courseId);
+        buttonUpdateCourse = (Button) findViewById(R.id.buttonUpdateCourse);
+        buttonDeleteCourse = (Button) findViewById(R.id.buttonDeleteCourse);
     }
 
     private void getCourseInfo () {
-        Collection collection = Collection.getInstance();
-
         Intent intent = getIntent();
-        int indexOfCourse = intent.getIntExtra(CourseListActivity.KEY,0);
+        String listContent = intent.getStringExtra(CourseListActivity.KEY);
 
-        int semester = collection.getCourse(indexOfCourse).getSemester();
-        int grade = collection.getCourse(indexOfCourse).getGrade();
-        int credit = collection.getCourse(indexOfCourse).getCredit();
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(listContent);
 
-        courseName.setText(collection.getCourse(indexOfCourse).getCourseName());
+        listCourseId = Integer.parseInt(m.replaceAll("").trim());
+        String listCourseName = courseDao.getCourseNameById(listCourseId);
+        String listCourseDetail = courseDao.getCourseDetailById(listCourseId);
+        int semester = courseDao.getCourseSemesterById(listCourseId);
+        int grade = courseDao.getCourseGradeById(listCourseId);
+        int credit = courseDao.getCourseCreditById(listCourseId);
+
+        courseName.setText(listCourseName);
         courseSemester.setText(String.valueOf(semester));
         courseGrade.setText(String.valueOf(grade));
         courseCredit.setText(String.valueOf(credit));
-        courseExtraInfo.setText(collection.getCourse(indexOfCourse).getCourseDetail());
-
+        courseDetail.setText(listCourseDetail);
+        courseId.setText(String.valueOf(listCourseId));
     }
+
+    private View.OnClickListener updateCourse = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String newCourseName = courseName.getText().toString();
+            String newCourseDetail = courseDetail.getText().toString();
+            int newSemester = Integer.parseInt(courseSemester.getText().toString());
+            int newGrade = Integer.parseInt(courseGrade.getText().toString());
+            int newCredit = Integer.parseInt(courseCredit.getText().toString());
+
+            Course course = new Course(newCourseName,newSemester,newGrade,newCredit,newCourseDetail);
+            course.setId(listCourseId);
+            courseDao.updateCourse(course);
+            showToast("Update succeeded!");
+            //CourseDetailsActivity.this.finish();
+        }
+    };
+
+    private View.OnClickListener deleteCourse = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Course course = new Course("",0,0,0,"");
+            course.setId(listCourseId);
+            courseDao.deleteCourse(course);
+            showToast("This course has been deleted!");
+            CourseDetailsActivity.this.finish();
+        }
+    };
+
+    private void showToast(String message){
+        Toast toast = Toast.makeText(getApplicationContext(), message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
 }
