31754ae0f099999d6333cb7b22b56b8364257056
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2021-03-07 20:31:14 +0200

    Added comments

--- app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
+++ app/src/main/java/fi/metropolia/team4studyprogressmanagement/fragment/PagerAdapter.java
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+/**
+ * Create PagerAdapter for ViewPager in TabActivity
+ */
+
 public class PagerAdapter extends FragmentPagerAdapter {
 
     private int numberOfTabs;
-
+//create constructor of PagerAdapter
     public  PagerAdapter(@NonNull FragmentManager fm, int behavior, int numberOfTabs){
         super(fm,behavior);
         this.numberOfTabs = numberOfTabs;
